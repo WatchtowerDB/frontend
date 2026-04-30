f8e77dece04e687bc26a5c7d830793cd441f7eb5
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-04-30 03:16:30 +0300

    fix: moved the logo to correct position

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,7 +14,7 @@ import { Button } from "../ui/button"
 
 export function Navbar() {
   return (
-    <NavigationMenu className="flex w-full max-w-none justify-between ps-5">
+    <NavigationMenu className="flex w-full max-w-none justify-between ps-2">
       {/* Logo */}
       <div className="me-1 mt-1">
         <Logo width={35} height={35} />
