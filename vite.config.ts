e60561d1cd68feeb9c8e59754e87fabe82ceb3fb
NightSparky <hossien360@hotmail.com>
2026-05-29 00:15:18 +0300

    fix: the mess that was color transition

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // or "0.0.0.0"
+  },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   resolve: {
     alias: {
