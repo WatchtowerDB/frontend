730a10496d644d67d04e6c31f7225d13a5a8f422
NightSparky <hossien360@hotmail.com>
2026-05-31 04:43:51 +0300

    feat: migrated to a better library to handle re-authentications

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,8 @@
 import { toAPIError } from "@/lib/utils"
 import axios from "axios"
+import createAuthRefreshInterceptor from "axios-auth-refresh"
 import { useAuthStore } from "../stores/useAuthStore"
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
@@ -16,36 +18,33 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => response,
-  async (error) => {
-    const prevRequest = error?.config
-    const isLoginRequest =
-      prevRequest?.url?.includes("/auth/") && !prevRequest?.url?.includes("/refresh/") // so it doesn't affect login page
-    if (error?.response?.status === 401 && !prevRequest?.sent && !isLoginRequest) {
-      console.log("Hello hello!", error.response.status)
-      prevRequest.sent = true
-
-      try {
-        console.log("attempting to refresh token")
-        const response = await axios.post(
-          `${import.meta.env.VITE_BACKEND_URL}/auth/refresh/`,
-          {},
-          { withCredentials: true },
-        )
-
-        const { access } = response.data
-        useAuthStore.getState().setAccessToken(access)
-
-        prevRequest.headers.Authorization = `Bearer ${access}`
-        return api(prevRequest)
-      } catch (err) {
-        console.log("axiosinstance reports here", err)
-        useAuthStore.getState().logout()
-        return Promise.reject(err)
-      }
-    }
+  (error) => {
+    if (error?.response?.status === 401) return Promise.reject(error) // let axios-auth-refresh handle it instead of turning it into an APIError
     return Promise.reject(toAPIError(error))
-    // Whenever any component is handling an api fetch error, add const apiError = error as APIError for graceful handling.
   },
 )
+
+// Surely you understand that this has to stay any.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const refreshAuthLogic = async (failedRequest: any) => {
+  try {
+    console.log("refreshAuthLogic firing", failedRequest)
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/auth/refresh/`,
+      {},
+      { withCredentials: true },
+    )
+    console.log("refresh response", response.data)
+    const { access } = response.data
+    useAuthStore.getState().setAccessToken(access)
+    failedRequest.response.config.headers.Authorization = `Bearer ${access}`
+  } catch (err) {
+    useAuthStore.getState().logout()
+    return Promise.reject(toAPIError(err))
+    // Whenever any component is handling an api fetch error, add const apiError = error as APIError for graceful handling.
+  }
+}
+
+createAuthRefreshInterceptor(api, refreshAuthLogic)
 
 export default api
