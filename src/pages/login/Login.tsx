7d58aa24cfbbf32ada9f9e77428fb262ef7918fa
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-06-22 07:56:02 +0300

    feat: cool gradient bg

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/Logo"
+import { MeshGradientBackground } from "@/components/ui/mesh-gradient-background"
 import { useAuthStore } from "@/stores/useAuthStore"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
@@ -15,7 +16,13 @@ export default function Login() {
   }, [isAuthenticated, navigate])
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <MeshGradientBackground
+        className="-z-50"
+        colors={["#004354", "#004354", "#0d718b", "#0d718b"]}
+        speed={1.5}
+        backgroundColor={"#101010"}
+      />
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="text-primary-foreground flex size-12 items-center justify-center rounded-md">
